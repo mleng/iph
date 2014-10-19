0c9c63e51b666757e8394bd4380c69b006853281
Marco Len <mlen@dichter-neira.com>
2014-10-19 11:13:12 +0000

    Changes made by Marco Len (mlen@dichter-neira.com).

--- phiApp/scripts/app/settings.js
+++ phiApp/scripts/app/settings.js
@@ -5,7 +5,7 @@
 var appSettings = {
 
     everlive: {
-        apiKey: '$EVERLIVE_API_KEY$', // Put your Backend Services API key here
+        apiKey: 'NMeHLfYpt4oiwPVD', // Put your Backend Services API key here
         scheme: 'http'
     },
 
